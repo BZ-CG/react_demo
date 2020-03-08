d5e1213ea373f2b6cb72ffb61575302668eba12e
CG-PC <1245978718@qq.com>
2020-03-08 21:40:18 +0800

    echarts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Button} from "antd";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import Login from "./pages/login/login";
 import Admin from "./pages/admin/admin";
 
 function App() {
   return (
         <BrowserRouter>
+                <Redirect exact from="/" to="/login" />
                 <Route path="/login" component={Login}/>
                 <Route path="/admin" component={Admin}/>
         </BrowserRouter>
--- src/pages/charts/Bar.jsx
+++ src/pages/charts/Bar.jsx
@@ -1,10 +1,64 @@
 import React, {Component} from "react";
+import ReactEcharts from 'echarts-for-react';
+import {Button, Card} from "antd";
 
 export default class Bar extends Component {
+
+    state = {
+        sale: [5, 20, 36, 10, 10, 20],
+        stock: [15, 30, 16, 20, 50, 70]
+    }
+
+
+    update = () => {
+        this.setState(state => ({
+            sale: state.sale.map(sale => sale +1),
+            stock: state.stock.map(stock => stock-1)
+        }))
+    };
+
+    /**
+     * 柱状图的配置对象
+     */
+    getOption = (sale, stock) => {
+        return {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量', "库存"]
+            },
+            xAxis: {
+                data: ["小米9","iPhone XR","iPhone 11 Pro Max","Macbook Pro","ASUS450J","雷柏"]
+            },
+            yAxis: {},
+            series: [
+                {
+                    name: '销量',
+                    type: 'bar',
+                    data: sale
+                },
+                {
+                    name: '库存',
+                    type: 'bar',
+                    data: stock
+                }
+            ]
+        }
+    };
     render() {
+        const {sale, stock} = this.state;
         return (
             <div>
-                Bar
+
+                <Card>
+                    <Button type="primary" onClick={this.update}>更新</Button>
+                </Card>
+
+                <Card title="柱状图">
+                    <ReactEcharts option={this.getOption(sale, stock)} />
+                </Card>
             </div>
         )
     }
